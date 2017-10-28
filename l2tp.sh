aeaebf8c797d57f205b0fdcab53106705c5b78a9
wn789 <768187171@qq.com>
2017-10-28 18:39:21 +0800

    Add files via upload

--- l2tp.sh
+++ l2tp.sh
@@ -10,6 +10,7 @@ export PATH
 cur_dir=`pwd`
 
 libreswan_filename="libreswan-3.20"
+download_root_url="http://dl.teddysun.com/files"
 
 rootness(){
     if [[ $EUID -ne 0 ]]; then
@@ -137,8 +138,6 @@ is_64bit(){
 }
 
 download_file(){
-    local download_root_url="http://dl.teddysun.com/files"
-
     if [ -s ${1} ]; then
         echo "$1 [found]"
     else
@@ -308,8 +307,10 @@ install_l2tp(){
         compile_install
     elif check_sys packageManager yum; then
         echo "Adding the EPEL repository..."
-        yum -y install epel-release
+        yum -y install epel-release yum-utils
         [ ! -f /etc/yum.repos.d/epel.repo ] && echo "Install EPEL repository failed, please check it." && exit 1
+        yum-config-manager --enable epel
+        echo "Adding the EPEL repository complete..."
 
         if centosversion 7; then
             yum -y install ppp libreswan xl2tpd firewalld
